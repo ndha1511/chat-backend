2f62fede81f56e6bc5a2eb5af72cd6fc27cc92c3
Nguyen Dinh Hoang Anh <ndha1115@gmail.com>
2024-01-14 10:57:09 +0700

    create base project

---
+++ src/test/java/com/project/zalobackend/ZaloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zalobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
