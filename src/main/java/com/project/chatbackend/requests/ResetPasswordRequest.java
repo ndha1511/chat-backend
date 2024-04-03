b858ca552390b06754142fb49f6293c441e84b48
Nguyen Dinh Hoang Anh <ndha1115@gmail.com>
2024-04-03 22:35:19 +0700

    add change password and reset password

---
+++ src/main/java/com/project/chatbackend/requests/ResetPasswordRequest.java
@@ -0,0 +1,23 @@
+package com.project.chatbackend.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResetPasswordRequest {
+    @NotBlank(message = "email is required")
+    @Pattern(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$", message = "email is invalid")
+    private String email;
+    @NotBlank(message = "otp is required")
+    @Size(min = 6, max = 6, message = "otp must be 6 characters")
+    private String otp;
+    @NotBlank(message = "new password is required")
+    @Size(min = 6, message = "new password must be 6 characters or more")
+    private String newPassword;
+    @NotBlank(message = "confirm new password is required")
+    private String confirmNewPassword;
+}
--- src/main/java/com/project/chatbackend/services/IUserService.java
+++ src/main/java/com/project/chatbackend/services/IUserService.java
@@ -2,8 +2,7 @@ package com.project.chatbackend.services;
 
 import com.project.chatbackend.exceptions.DataNotFoundException;
 import com.project.chatbackend.models.User;
-import com.project.chatbackend.requests.UseRegisterRequest;
-import com.project.chatbackend.requests.UserLoginRequest;
+import com.project.chatbackend.requests.*;
 import com.project.chatbackend.responses.LoginResponse;
 import com.project.chatbackend.responses.UserLoginResponse;
 
@@ -11,19 +10,15 @@ import com.project.chatbackend.responses.UserLoginResponse;
 public interface IUserService {
     User createUser(UseRegisterRequest useRegisterRequest) throws Exception;
     LoginResponse login(UserLoginRequest userLoginRequest) throws Exception;
-
     LoginResponse refreshToken(String refreshToken) throws Exception;
-
     UserLoginResponse findByPhoneNumber(String phoneNumber) throws Exception;
-
     UserLoginResponse findByEmail(String email) throws Exception;
-
-
-
     UserLoginResponse findById(String id) throws Exception;
-
-
     User findUserByEmail(String email) throws DataNotFoundException;
-
     boolean deleteUserByEmail(String email);
+
+    boolean isValidOTP(OtpValidRequest otpValidRequest);
+
+    boolean changePassword(ChangePasswordRequest changePasswordRequest);
+    boolean resetPassword(ResetPasswordRequest resetPasswordRequest);
 }
