779d90f32277fc877cea4b9418348b157333992f
Nguyen Dinh Hoang Anh <ndha1115@gmail.com>
2024-04-07 20:40:46 +0700

    update upload performance

--- src/main/java/com/project/chatbackend/services/S3UploadService.java
+++ src/main/java/com/project/chatbackend/services/S3UploadService.java
@@ -1,6 +1,7 @@
 package com.project.chatbackend.services;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
@@ -31,23 +32,42 @@ public class S3UploadService {
 
     public Map<String, String> uploadFile(MultipartFile file) throws IOException {
         log.info("start uploading at " + new Date(System.currentTimeMillis()));
-        File fileObj = null;
-        try {
-            fileObj = convertMultiPartFileToFile(file);
-            String fileName = System.currentTimeMillis() + "_" + Objects.
-                    requireNonNull(file.getOriginalFilename())
-                    .replace(" ", "-");
-            s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
-            fileObj.delete();
-            log.info("end upload at" + new Date(System.currentTimeMillis()));
-            Map<String, String> fileInfo = new HashMap<>();
-            fileInfo.put(fileName, "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + fileName);
-            return fileInfo;
-        } catch (Exception io) {
-            assert fileObj != null;
-            fileObj.delete();
-            throw io;
-        }
+//        File fileObj = null;
+//        try {
+//            fileObj = convertMultiPartFileToFile(file);
+//            String fileName = System.currentTimeMillis() + "_" + Objects.
+//                    requireNonNull(file.getOriginalFilename())
+//                    .replace(" ", "-");
+//            s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+//            fileObj.delete();
+//            log.info("end upload at" + new Date(System.currentTimeMillis()));
+//            Map<String, String> fileInfo = new HashMap<>();
+//            fileInfo.put(fileName, "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + fileName);
+//            return fileInfo;
+//        } catch (Exception io) {
+//            assert fileObj != null;
+//            fileObj.delete();
+//            throw io;
+//        }
+        String fileName = file.getOriginalFilename();
+        String key = generateUniqueKey(fileName);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+        metadata.setLastModified(new Date());
+        PutObjectRequest request = new PutObjectRequest(bucketName, key, file.getInputStream(), metadata);
+        s3Client.putObject(request);
+
+        log.info("File uploaded to S3 - Key: {}, Bucket: {}", key, bucketName);
+        Map<String, String> fileInfo = new HashMap<>();
+        fileInfo.put(key, "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key);
+        return fileInfo;
+    }
+
+    private String generateUniqueKey(String originalFileName) {
+        // Tạo key duy nhất dựa trên tên file gốc hoặc sử dụng UUID
+        // Ví dụ: return UUID.randomUUID().toString();
+        return System.currentTimeMillis() + "_" + originalFileName;
     }
 
 
@@ -65,14 +85,12 @@ public class S3UploadService {
     }
 
 
-    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
-        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
-            fos.write(file.getBytes());
-        } catch (Exception ex) {
-            convertedFile.delete();
-            throw ex;
-        }
-        return convertedFile;
-    }
+//    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
+//        String tempDir = System.getProperty("java.io.tmpdir");
+//        File convertedFile = new File(tempDir, Objects.requireNonNull(file.getOriginalFilename()));
+//        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+//            fos.write(file.getBytes());
+//        }
+//        return convertedFile;
+//    }
 }
